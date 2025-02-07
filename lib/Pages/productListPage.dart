86baeae067ae4126fc0b2fc740592a5617ba3f04
basilpc <basilpc1999@gmail.com>
2025-02-07 18:39:55 +0530

    some changes

--- lib/Pages/productListPage.dart
+++ lib/Pages/productListPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:productlist/Pages/productApiIntegration.dart';
 import 'ProductDetailPage.dart';
 
@@ -29,16 +28,15 @@ class _ProductListPageState extends State<ProductListPage> {
   }
 
   void filterSearchResults(String query) {
-    List<dynamic> SearchList = [];
+    List<dynamic> searchList = [];
     if (query.isNotEmpty) {
-      SearchList = allProducts.where((product) =>
+      searchList = allProducts.where((product) =>
           product['title'].toLowerCase().contains(query.toLowerCase())).toList();
     } else {
-      SearchList = allProducts;
-      print(("serchList$SearchList"));
+      searchList = allProducts;
     }
     setState(() {
-      filteredProducts = SearchList;
+      filteredProducts = searchList;
     });
   }
 
@@ -46,93 +44,93 @@ class _ProductListPageState extends State<ProductListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: const Text('ProductList APP', style: TextStyle(fontWeight: FontWeight.bold))),
+        title: const Text('Product List APP', style: TextStyle(fontWeight: FontWeight.bold)),
         backgroundColor: Colors.deepPurple,
-      ),
-      body: Column(
-        children: [
-          Padding(
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(60), // Extending AppBar height
+          child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: searchController,
               onChanged: (value) => filterSearchResults(value),
               decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
                 labelText: 'Search',
                 hintText: 'Search by product title',
                 prefixIcon: const Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
                 ),
               ),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.7,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.7,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: filteredProducts.length,
+          itemBuilder: (context, index) {
+            final product = filteredProducts[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductDetailPage(productId: product['id']),
+                  ),
+                );
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                itemCount: filteredProducts.length,
-                itemBuilder: (context, index) {
-                  final product = filteredProducts[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProductDetailPage(productId: product['id']),
-                        ),
-                      );
-                    },
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                elevation: 5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
+                      child: Image.network(
+                        product['image'],
+                        height: 120,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
                       ),
-                      elevation: 5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          ClipRRect(
-                            borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
-                            child: Image.network(
-                              product['image'],
-                              height: 120,
-                              width: double.infinity,
-                              fit: BoxFit.cover,
-                            ),
+                          Text(
+                            product['title'],
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.center,
                           ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Text(
-                                  product['title'],
-                                  style: const TextStyle(fontWeight: FontWeight.bold),
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.center,
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  '\$${product['price']}',
-                                  style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.deepPurple),
-                                ),
-                              ],
-                            ),
+                          const SizedBox(height: 5),
+                          Text(
+                            '\$${product['price']}',
+                            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.deepPurple),
                           ),
                         ],
                       ),
                     ),
-                  );
-                },
+                  ],
+                ),
               ),
-            ),
-          ),
-        ],
+            );
+          },
+        ),
       ),
     );
   }
